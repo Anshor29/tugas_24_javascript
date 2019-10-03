e64f9c2866be9699d018b148cf1e798e63ead4e9
Anshor <anshorjihadi@gmail.com>
2019-10-03 12:59:53 +0700

    Tugas 24 Js

---
+++ tugas24.js
@@ -0,0 +1,5 @@
+var x = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+console.log("Sebelumnya :", x.toString());
+console.log("Ascending :", x.sort().toString());
+console.log("Descending :", x.sort().reverse().toString());
